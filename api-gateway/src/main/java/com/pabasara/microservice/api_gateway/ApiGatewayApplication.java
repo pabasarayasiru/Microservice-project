e1ad3f4f8f0f2128aac3b2b69c3638a8546cb4dc
pabasarayasiru <pabasara.yasiru@gmail.com>
2026-01-08 10:01:56 +0530

    changed minor issues

--- api-gateway/src/main/java/com/pabasara/microservice/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/pabasara/microservice/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.api_gateway;
+package com.pabasara.microservice.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- api-gateway/src/test/java/com/pabasara/microservice/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/pabasara/microservice/api_gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.api_gateway;
+package com.pabasara.microservice.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
